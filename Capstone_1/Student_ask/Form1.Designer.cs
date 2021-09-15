383e8e4f05ce26d531a8ff7d792c4851f2d7d924
고현정 <080821@naver.com>
2021-09-16 03:34:03 +0900

    [ * ] 0916 수정

--- Capstone_1/Student_ask/Form1.Designer.cs
+++ Capstone_1/Student_ask/Form1.Designer.cs
@@ -29,19 +29,36 @@ namespace Student_Ask
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 203);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 318);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(320, 208);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "사진 확대";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Capstone_1/Student_ask/Form1.cs
+++ Capstone_1/Student_ask/Form1.cs
@@ -16,5 +16,21 @@ namespace Student_Ask
         {
             InitializeComponent();
         }
+
+        public Image img
+        {
+            //외부로부터 Image참조 설정을 위한 속성
+            set; get;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //폼이 로딩될때 Image와 크기 설정
+            pictureBox1.Image = img;
+            pictureBox1.Size = img.Size;
+
+        }
+
+       
     }
 }
