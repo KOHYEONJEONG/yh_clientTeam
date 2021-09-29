9158d52a4a4c33bf7141290a0b7079c306007152
Rwhisper <73861946+Rwhisper@users.noreply.github.com>
2021-09-29 11:42:32 +0900

    [*] LoginFormImageSetting

--- Program/LoginMain/LoginForm.Designer.cs
+++ Program/LoginMain/LoginForm.Designer.cs
@@ -47,17 +47,20 @@ namespace LoginMain
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(364, 217);
+            this.panel1.Size = new System.Drawing.Size(283, 163);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Image = global::LoginMain.Properties.Resources.img_sbmark_large;
-            this.pictureBox1.Location = new System.Drawing.Point(79, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(61, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(212, 211);
+            this.pictureBox1.Size = new System.Drawing.Size(165, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -66,9 +69,10 @@ namespace LoginMain
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label1.Location = new System.Drawing.Point(49, 260);
+            this.label1.Location = new System.Drawing.Point(38, 195);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 23);
+            this.label1.Size = new System.Drawing.Size(53, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "UserID";
             // 
@@ -77,42 +81,47 @@ namespace LoginMain
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label2.Location = new System.Drawing.Point(49, 352);
+            this.label2.Location = new System.Drawing.Point(38, 264);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 23);
+            this.label2.Size = new System.Drawing.Size(73, 19);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password";
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(49, 326);
+            this.panel2.Location = new System.Drawing.Point(38, 244);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(250, 1);
+            this.panel2.Size = new System.Drawing.Size(195, 1);
             this.panel2.TabIndex = 3;
             // 
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(49, 420);
+            this.panel3.Location = new System.Drawing.Point(38, 315);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(250, 1);
+            this.panel3.Size = new System.Drawing.Size(195, 1);
             this.panel3.TabIndex = 4;
             // 
             // txt_id
             // 
             this.txt_id.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_id.Location = new System.Drawing.Point(49, 296);
+            this.txt_id.Location = new System.Drawing.Point(38, 222);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(125, 20);
+            this.txt_id.Size = new System.Drawing.Size(97, 16);
             this.txt_id.TabIndex = 5;
             // 
             // txt_pw
             // 
             this.txt_pw.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_pw.Location = new System.Drawing.Point(49, 387);
+            this.txt_pw.Location = new System.Drawing.Point(38, 290);
+            this.txt_pw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_pw.Name = "txt_pw";
-            this.txt_pw.Size = new System.Drawing.Size(125, 20);
+            this.txt_pw.Size = new System.Drawing.Size(97, 16);
             this.txt_pw.TabIndex = 6;
             // 
             // button1
@@ -122,9 +131,10 @@ namespace LoginMain
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(143, 442);
+            this.button1.Location = new System.Drawing.Point(111, 332);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 35);
+            this.button1.Size = new System.Drawing.Size(73, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -132,10 +142,10 @@ namespace LoginMain
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(365, 495);
+            this.ClientSize = new System.Drawing.Size(284, 371);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txt_pw);
             this.Controls.Add(this.txt_id);
@@ -145,6 +155,7 @@ namespace LoginMain
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "LoginForm";
             this.Text = "로그인";
             this.Load += new System.EventHandler(this.Form1_Load);
