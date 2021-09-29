67c89376f279a8217a4d48f69d21af8f46d278b0
Jyeemin <9192939@naver.com>
2021-09-29 16:02:25 +0900

    main store

---
+++ Program (1)/Program/ProgramMain/Professor/ImageForm.designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace ProgramMain
+{
+    partial class ImageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbScreenshot = new System.Windows.Forms.PictureBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreenshot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbScreenshot
+            // 
+            this.pbScreenshot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbScreenshot.Location = new System.Drawing.Point(35, 37);
+            this.pbScreenshot.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbScreenshot.Name = "pbScreenshot";
+            this.pbScreenshot.Size = new System.Drawing.Size(448, 339);
+            this.pbScreenshot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbScreenshot.TabIndex = 0;
+            this.pbScreenshot.TabStop = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.lblTime.Location = new System.Drawing.Point(275, 19);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(66, 15);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "쵤영시간 : ";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(35, 19);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 12);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "이름 : ";
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(129, 19);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(41, 12);
+            this.lblNum.TabIndex = 2;
+            this.lblNum.Text = "학번 : ";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(408, 385);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 22);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "확인";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // ImageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(527, 418);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.pbScreenshot);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ImageForm";
+            this.Text = "고구마20210903의 스크린샷";
+            this.Load += new System.EventHandler(this.ImageForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbScreenshot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbScreenshot;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
