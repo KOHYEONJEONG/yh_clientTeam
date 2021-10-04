9d7884610f445a0c1cebf081576f9d243ff06c0e
ko <080821@naver.com>
2021-10-04 23:31:23 +0900

    로그인,질문폼 수정

---
+++ ProgramMain/Student/Student_askf.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace ProgramMain
+{
+    partial class Student_askf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_screenCopy = new System.Windows.Forms.Button();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.chkAsk = new System.Windows.Forms.CheckBox();
+            this.chkImage = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_screenCopy);
+            this.groupBox1.Controls.Add(this.btn_select);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 308);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "이미지";
+            // 
+            // btn_screenCopy
+            // 
+            this.btn_screenCopy.Location = new System.Drawing.Point(220, 274);
+            this.btn_screenCopy.Name = "btn_screenCopy";
+            this.btn_screenCopy.Size = new System.Drawing.Size(94, 29);
+            this.btn_screenCopy.TabIndex = 2;
+            this.btn_screenCopy.Text = "화면캡쳐";
+            this.btn_screenCopy.UseVisualStyleBackColor = true;
+            this.btn_screenCopy.Click += new System.EventHandler(this.btn_screenCopy_Click);
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(120, 274);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(94, 29);
+            this.btn_select.TabIndex = 1;
+            this.btn_select.Text = "선택";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 246);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(1, 316);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(435, 271);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "질문";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(414, 234);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"JPEG|*.jpg|PNG|*.png|Bitmap|*.bmp“ ";
+            this.openFileDialog1.ValidateNames = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSend);
+            this.groupBox3.Controls.Add(this.chkAsk);
+            this.groupBox3.Controls.Add(this.chkImage);
+            this.groupBox3.Location = new System.Drawing.Point(1, 586);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(431, 44);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "전송대상";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(287, 14);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(94, 29);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // chkAsk
+            // 
+            this.chkAsk.AutoSize = true;
+            this.chkAsk.Location = new System.Drawing.Point(220, 14);
+            this.chkAsk.Name = "chkAsk";
+            this.chkAsk.Size = new System.Drawing.Size(61, 24);
+            this.chkAsk.TabIndex = 5;
+            this.chkAsk.Text = "질문";
+            this.chkAsk.UseVisualStyleBackColor = true;
+            // 
+            // chkImage
+            // 
+            this.chkImage.AutoSize = true;
+            this.chkImage.Location = new System.Drawing.Point(120, 14);
+            this.chkImage.Name = "chkImage";
+            this.chkImage.Size = new System.Drawing.Size(76, 24);
+            this.chkImage.TabIndex = 4;
+            this.chkImage.Text = "이미지";
+            this.chkImage.UseVisualStyleBackColor = true;
+            // 
+            // Student_askf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(435, 634);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Student_askf";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_screenCopy;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.CheckBox chkAsk;
+        private System.Windows.Forms.CheckBox chkImage;
+    }
+}
