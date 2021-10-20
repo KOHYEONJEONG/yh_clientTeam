ee25c169ab1b54dfd2326e9f3fcb264498fdc7e9
shc6260 <shc6260@gmail.com>
2021-10-20 15:29:20 +0900

    병합

---
+++ ProgramMain/LoginForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ProgramMain
+{
+    public partial class LoginForm : MetroFramework.Forms.MetroForm 
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        Boolean exit = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //글자수 제한
+            this.txt_id.MaxLength = 32;
+            this.txt_pw.MaxLength = 32;
+
+            this.StyleManager = metroStyleManager1;
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;
+            //chkRemember.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(txt_id.Text.Equals("") || txt_pw.Text.Equals(""))
+            {
+                if (txt_id.Text.Equals(""))
+                {
+                    MessageBox.Show("ID를 입력해주세요.");
+                    txt_id.Focus();
+                }
+                else
+                {
+                    if (txt_pw.Text.Equals(""))
+                    {
+                        MessageBox.Show("password를 입력해주세요.");
+                        txt_pw.Focus();
+                    }
+                }
+            }
+            else
+            {
+
+                //  pw 유효성 검사 후 로그인 
+                if ( CheckPassword() )  //!
+                {
+
+                    if (txt_id.Text.Equals("p") && txt_pw.Text.Equals("1234"))
+                    {
+                        exit = false;
+
+                        MessageBox.Show("교수프로그램", "확인", MessageBoxButtons.OK);
+                        //ProfesserMain professerMain = new ProfesserMain();
+                        //professerMain.Show();
+                        (new ProfesserMain()).Show();
+                        this.Close();
+                      
+                    }
+                    else
+                    {
+                        if (txt_id.Text.Equals("s") && txt_pw.Text.Equals("1234"))
+                        {
+                            exit = false;
+
+                            this.Hide();
+                            MessageBox.Show("학생프로그램", "확인", MessageBoxButtons.OK);
+                            //student_main student = new student_main();
+                            //student.Show();
+                           
+                            (new student_main()).Show();
+
+                            this.Close();
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    //특수문자 중 !,@외 문자가 들어 있을 경우.
+                    txt_pw.Text = "";
+                    txt_pw.Focus();
+                }
+
+            }
+          
+
+        }// 로그인 버튼
+
+
+        //유효성검사
+        public bool CheckPassword()
+        {
+            string idChecker = Regex.Replace(txt_pw.Text, @"[^0-9a-zA-Z!@ ]{1,32}","", RegexOptions.Singleline);
+
+
+            if (txt_pw.Text.Equals(idChecker) == false)
+            {
+
+                txt_pw.Text.Remove(0, txt_pw.Text.Length);
+
+                txt_pw.Text = "";
+                txt_pw.Focus();
+                MessageBox.Show("특수문자(!,@는 제외)\n공백은 허용되지 않습니다..");
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+
+
+
+        }//CheckPassword
+
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!exit)
+            {
+                return;
+            }
+          
+            else
+            {
+
+                if (MessageBox.Show("알림창", "학습 도우미 프로그램을 종료하시나요?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+           
+            //중요(스레드를 종료해야함)
+            //Application.Exit();
+            
+        }
+    }
+}
---
+++ ProgramMain/Student/Student_askf.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramMain
+{
+    public partial class Student_askf : MetroFramework.Forms.MetroForm
+    {
+        public Student_askf()
+        {
+            InitializeComponent();
+        }
+
+        /*    private void btn_select_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    }
+
+                }catch(Exception ex)
+                {
+                    //Filter속성에서 이미지를 걸러내기 때문에 안적어도 됨.
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }*/
+
+
+        Bitmap bmp = null;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PctClick frm = new PctClick();
+            frm.img = pictureBox1.Image;
+            frm.ShowDialog();
+        }
+
+        private void btn_screenCopy_Click(object sender, EventArgs e)
+        {
+            //스크린샷 폼은 사라지게.
+            this.Hide();
+            Thread.Sleep(500);
+
+            //스크린샷 버튼
+            Screen scr = Screen.PrimaryScreen;
+
+            //듀얼 모니터
+            //Screen scr2 = Screen.AllScreens[1];
+
+            //좌표
+            Rectangle rect = scr.Bounds;
+
+            //확면을 복사해 이미지 저장
+            bmp = new Bitmap(rect.Width, rect.Height);
+
+
+            //이미지를 변경하기 위해 Graphics객체를 가져옴.
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(rect.Left, rect.Top, 0, 0, rect.Size);
+            }
+
+           
+                pictureBox1.Image = (Image)bmp;
+
+                //Image.FromFile(openFileDialog1.FileName);
+
+
+            this.Show();
+        }
+
+        /*
+              //폴더가 경로 있는지
+              if( (Directory.Exists(@"C:\Temp2")==false)){
+                  Directory.CreateDirectory(@"C:\Temp2");
+              }
+              else { 
+
+
+                      //스크린샷 저장 경로
+                      bmp.Save(@"C:\Temp2\scr.png", ImageFormat.Png);
+
+                      bmp.Dispose();//스크린 복사후 더이상 Graphics를 사용안해 해제.
+
+
+                  //스크린샷 끝난후 폼 다시 보여주기.
+                  this.Show();
+
+
+
+              }
+          }
+      */
+
+       public byte[] bitmapToByteArray( Bitmap bitmap ) 
+        {
+            byte[] result = null;
+            if (bitmap != null)
+            {
+                MemoryStream stream = new MemoryStream();
+                bitmap.Save(stream, bitmap.RawFormat);
+                result = stream.ToArray();
+            }
+            else
+            {
+                Console.WriteLine("Bitmap is null.");
+            }
+            return result;
+
+        }
+
+
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            byte[] imgByte = bitmapToByteArray(bmp); 
+
+
+            if (chkImage.Checked && chkAsk.Checked)
+            {
+                MessageBox.Show("이미지,질문 전송완료");
+            }
+            else
+            {
+                if (chkImage.Checked)
+                {
+                    MessageBox.Show("이미지 전송완료");
+                }
+                else
+                {
+                    if (chkAsk.Checked)
+                    {
+                        MessageBox.Show("질문 전송완료");
+                    }
+                }
+            }
+        }
+
+        private void Student_askf_Load(object sender, EventArgs e)
+        {
+            /*this.StyleManager = metroStyleManager1;
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;*/
+        }
+    }
+}
